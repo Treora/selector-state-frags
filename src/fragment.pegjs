987afeccd1b60dc0df85a8e0b67f93c6a1d35c6f
Gerben <gerben@treora.com>
2017-05-03 12:59:29 -0700

    Style consistency

--- src/fragment.pegjs
+++ src/fragment.pegjs
@@ -15,20 +15,20 @@
 
 
 start =
-	top
+    top
 
 top
-	= "state" "(" p:params ")"      { return { state: p } }
+    = "state" "(" p:params ")"      { return { state: p } }
     / "selector" "(" p:params ")"   { return { selector: p } }
     / "state=" v:value              { return { state: v } }
     / "selector=" v:value           { return { selector: v } }
 
 params
-	= k1: key_value_pair k2:("," key_value_pair)*
-    	{
-        	var f = k1;
+    = k1: key_value_pair k2:("," key_value_pair)*
+        {
+            var f = k1;
             for( var i = 0; i < k2.length; i++ ) {
-            	f = collect(f, k2[i][1])
+                f = collect(f, k2[i][1])
             }
             return f;
         }
@@ -43,26 +43,26 @@ key_value_pair
             return { refinedBy: p }
          }
     / "startSelector=selector(" p:params ")"
-    	{
-        	return { startSelector: p };
+        {
+            return { startSelector: p };
         }
     / "endSelector=selector(" p:params ")"
-    	{
-        	return { endSelector: p };
+        {
+            return { endSelector: p };
         }
-	/ key:key "=" value:value
-    	 {
-         	var f = {};
+    / key:key "=" value:value
+        {
+            var f = {};
             var num = Number(value);
             f[key] = isNaN(num) ? decodeURIComponent(value): num;
             return f;
-         }
+        }
 
 key
-	= atom
+    = atom
 
 value
-	= atom
+    = atom
 
 atom
     = chars:validchar+ { return chars.join(""); }
--- src/index.js
+++ src/index.js
@@ -2,70 +2,70 @@ import Parser from './fragment.js'
 import map from 'lodash.map'
 
 export function uriToSelector(the_uri) {
-    var uri = undefined;
-	var fragment = "";
-	var splitted = the_uri.split('#');
+    var uri = undefined
+    var fragment = ''
+    var splitted = the_uri.split('#')
 
-	if( splitted.length == 2 ) {
-		uri      = splitted[0];
-		fragment = splitted[1]
-	} else {
-		fragment = the_uri
-	}
-	var selector = Parser.parse(fragment);
-	selector.source = uri;
-	return selector
+    if( splitted.length == 2 ) {
+        uri      = splitted[0]
+        fragment = splitted[1]
+    } else {
+        fragment = the_uri
+    }
+    var selector = Parser.parse(fragment)
+    selector.source = uri
+    return selector
 }
 
 export function selectorToUri(obj) {
 
-		function state_or_selector(obj) {
-			// Deciding whether the object is a selector or a state...
-			return (obj.type === "TimeState" || obj.type === "HttpRequestState") ? "state" : "selector";
-		}
+    function state_or_selector(obj) {
+        // Deciding whether the object is a selector or a state...
+        return (obj.type === 'TimeState' || obj.type === 'HttpRequestState') ? 'state' : 'selector'
+    }
 
-		function encode_value( val ) {
-			return encodeURI(val)
-				.replace(/#/g,'%23')
-				.replace(/,/g,'%2C')
-				.replace(/=/g,'%3D')
-				.replace(/%5B/gi,'[')
-				.replace(/%5D/gi,']')
-				.replace(/%3E/gi,'>')
-				.replace(/%3C/gi,'<')
-		}
+    function encode_value( val ) {
+        return encodeURI(val)
+            .replace(/#/g,'%23')
+            .replace(/,/g,'%2C')
+            .replace(/=/g,'%3D')
+            .replace(/%5B/gi,'[')
+            .replace(/%5D/gi,']')
+            .replace(/%3E/gi,'>')
+            .replace(/%3C/gi,'<')
+    }
 
-		function frag(fname, obj) {
-			function key_val(val, key) {
-				// Create either a key=val string or, for some specific key values
-				// calls recursively the fragment generator with that key.
-				// The only mini-complication is that the 'function' name for the recursion should
-				// be derived from the object (which is either a state for refinement, or a range selector)
-				// The unusual order of the arguments is because the function is to be used in _.map, which imposes
-				// this order
-				if( key === 'refinedBy' || key === "startSelector" || key === "endSelector" ) {
-					return key + '=' + frag(state_or_selector(val), val);
-				} else {
-					return key + '=' + encode_value(val)
-				}
-			}
+    function frag(fname, obj) {
+        function key_val(val, key) {
+            // Create either a key=val string or, for some specific key values
+            // calls recursively the fragment generator with that key.
+            // The only mini-complication is that the 'function' name for the recursion should
+            // be derived from the object (which is either a state for refinement, or a range selector)
+            // The unusual order of the arguments is because the function is to be used in _.map, which imposes
+            // this order
+            if( key === 'refinedBy' || key === 'startSelector' || key === 'endSelector' ) {
+                return key + '=' + frag(state_or_selector(val), val)
+            } else {
+                return key + '=' + encode_value(val)
+            }
+        }
 
-			if( typeof(obj) === 'string' ) {
-				// in practice: this is a uri
-				return fname + '=' + encode_value(obj)
-			} else {
-				return fname + "(" + map(obj, key_val) + ")"
-			}
-		}
+        if( typeof(obj) === 'string' ) {
+            // in practice: this is a uri
+            return fname + '=' + encode_value(obj)
+        } else {
+            return fname + '(' + map(obj, key_val) + ')'
+        }
+    }
 
-		var uri = obj.source;
-        var fragment;
-		if( obj.selector !== undefined ) {
-			fragment = frag("selector", obj.selector);
-		} else if (obj.state !== undefined ) {
-			fragment = frag("state", obj.state)
-		}
+    var uri = obj.source
+    var fragment
+    if ( obj.selector !== undefined ) {
+        fragment = frag('selector', obj.selector)
+    } else if ( obj.state !== undefined ) {
+        fragment = frag('state', obj.state)
+    }
 
-		return (uri !== undefined) ? uri + '#' + fragment : fragment
+    return (uri !== undefined) ? uri + '#' + fragment : fragment
 
 }
